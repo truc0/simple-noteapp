474d5ae4673e92327e7671103ba8f0dcad4a5eab
truc0 <22969604+truc0@users.noreply.github.com>
2022-01-20 11:41:09 +0800

    feat: add note model

--- note/models.py
+++ note/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default='')
+
+    class Meta:
+        ordering = ['created']
