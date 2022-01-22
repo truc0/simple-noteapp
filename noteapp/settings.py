3ef6e8d70af8839e228f9d382c06506e09f11d04
truc0 <22969604+truc0@users.noreply.github.com>
2022-01-22 20:42:55 +0800

    chore(settings): optimize settings for production

--- noteapp/settings.py
+++ noteapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from . import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t-s=c24x!xlpr576ikf8u4g6(s6j^er$t7#qb+arybsr*$i7kd'
+SECRET_KEY = config.SECRET_KEY
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getattr(config, 'DEBUG', False)
 
 ALLOWED_HOSTS = []
 
